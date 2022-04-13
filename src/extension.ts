a6170a8fdf90a25f8e75cc68c37053d66654d586
David Marshall <marshall.david78@gmail.com>
2022-04-13 18:42:51 +0000

    -

--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,112 @@
 import * as vscode from 'vscode'
 
-let statusBarItem: vscode.StatusBarItem
+let settings: {
+	showParentTasks: boolean,
+}
+
+let state: {
+	statusBarItem: vscode.StatusBarItem,
+} = {
+	statusBarItem: null,
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
-	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0,)
-	statusBarItem.name = 'todo-bar'
-	statusBarItem.command = 'todo-bar.clear'
-	statusBarItem.hide()
+	// Fetch settings
+	settings = vscode.workspace.getConfiguration('todo-bar') as unknown as typeof settings
 
-	updateTheme()
-	vscode.window.onDidChangeActiveColorTheme(updateTheme)
+	// Create statusBarItem
+	state.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0,)
+	state.statusBarItem.name = 'todo-bar'
+	state.statusBarItem.command = 'todo-bar.clear'
+	state.statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground')
+	state.statusBarItem.hide()
 
-	let disposable1 = vscode.commands.registerCommand('todo-bar.set-todo', setTodo)
-	let disposable2 = vscode.commands.registerCommand('todo-bar.clear', clearTodo)
+	// Register commands
+	let disposable1 = vscode.commands.registerCommand('todo-bar.set-todo', setTodo_Command)
+	let disposable2 = vscode.commands.registerCommand('todo-bar.clear', clearTodo_Command)
 
-	context.subscriptions.push(disposable1, disposable2, statusBarItem)
+	// Listen to changes
+	let disposable3 = vscode.workspace.onDidChangeConfiguration(() => {
+		displayTodo(state.statusBarItem.text)
+	})
+
+	context.subscriptions.push(disposable1, disposable2, disposable3, state.statusBarItem)
 }
 
-function setTodo() {
+function setTodo_Command() {
 	const activeEditor = vscode.window.activeTextEditor
 	if (activeEditor) {
-		const line = activeEditor.document.lineAt(activeEditor.selection.active.line)
-		const text = line.text.trim()
-		if (text == statusBarItem.text) {
-			clearTodo()
+		const text = fetchTodoText(activeEditor)
+		if (text == state.statusBarItem.text) {
+			clearTodo_Command()
 		} else {
-			statusBarItem.text = text
-			statusBarItem.tooltip = text
-			statusBarItem.show()
+			displayTodo(text)
 		}
 	}
 }
 
-function clearTodo() {
-	statusBarItem.text = ''
-	statusBarItem.hide()
+function clearTodo_Command() {
+	state.statusBarItem.text = ''
+	state.statusBarItem.hide()
 }
 
-function updateTheme() {
-	const theme = vscode.window.activeColorTheme
-	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground')
-
-	// switch (theme.kind) {
-	// 	case vscode.ColorThemeKind.Light:
-	// 	case vscode.ColorThemeKind.HighContrastLight:
-	// 		statusBarItem.color = new vscode.ThemeColor('errorForeground')
-	// 		break
-	// 	case vscode.ColorThemeKind.Dark:
-	// 	case vscode.ColorThemeKind.HighContrast:
-	// 		statusBarItem.color = '#ffffff'
-	// 		break
-	// }
-
+// Reads the selected line from the document, and optionally the parent tasks
+function fetchTodoText(activeEditor: vscode.TextEditor): string {
+	let lines: vscode.TextLine[] = []
+	if (settings.showParentTasks) {
+		lines = fetchParentLines(activeEditor.document, activeEditor.selection.active.line)
+	} else {
+		lines = [activeEditor.document.lineAt(activeEditor.selection.active.line)]
+	}
+	return lines
+		.map(line => line.text.trim())
+		.map(line => removeLeadingChars(line, [' ', '-']))
+		.reverse()
+		.join(' -> ')
 }
 
+function removeLeadingChars(text: string, symbols: string[]): string {
+	let i = 0
+	for (; i < symbols.length; i++) {
+		if (!symbols.includes(text[i])) {
+			break
+		}
+	}
+	return text.slice(i)
+}
+
+function fetchParentLines(document: vscode.TextDocument, mainLineNumber: number): vscode.TextLine[] {
+	let lines = []
+	let currentIndentationLevel = 999
+	for (let i = mainLineNumber; i >= 0; i--) {
+		const line = document.lineAt(i)
+		const lineIndentationLevel = indentationLevel(line)
+		if (lineIndentationLevel < currentIndentationLevel) {
+			currentIndentationLevel = lineIndentationLevel
+			lines.push(line)
+		}
+	}
+	return lines
+}
+
+function indentationLevel(line: vscode.TextLine) {
+	let res = 0
+	for (let i = 0; i < line.text.length; i++) {
+		if (line.text[i] == '\t') {
+			res++
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+function displayTodo(text: string) {
+	state.statusBarItem.text = text
+	state.statusBarItem.tooltip = text
+	state.statusBarItem.show()
+}
 
 export function deactivate() { }
+
