ef112da893d27cc0fe0ce8d76fe325ce7bd17582
David Marshall <marshall.david78@gmail.com>
2024-10-28 19:07:37 +0000

    window title: dont remember the last window title, just set it to undefined

--- src/window-title.ts
+++ src/window-title.ts
@@ -3,13 +3,12 @@ import * as vscode from 'vscode'
 export class WindowTitle {
 
 	windowConfig = vscode.workspace.getConfiguration('window')
-	previousWindowTitle = this.windowConfig.get('title')
 
 	set(title: string) {
 		this.windowConfig.update('title', title, vscode.ConfigurationTarget.WorkspaceFolder)
 	}
 
 	restore() {
-		this.windowConfig.update('title', this.previousWindowTitle, vscode.ConfigurationTarget.WorkspaceFolder)
+		this.windowConfig.update('title', undefined)
 	}
 }
